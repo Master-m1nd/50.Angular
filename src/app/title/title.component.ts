a0e953e1e99a68dd84c207cc306bfba7125448b8
Master-m1nd <lukachynetsdean@gmail.com>
2023-04-21 21:07:48 +0300

    first commit

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  template: `<h1 class="main">Main title</h1>`,
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent {}
